60f3767d055f646deae2d905ae8b1edd330ac02f
xportell <xportell@confluencia.eu>
2016-07-07 14:33:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ var api = new ParseServer({
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
+console.log('----api-----');
+console.log(api.cloud);
 
 var app = express();
 
