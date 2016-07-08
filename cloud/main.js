3d3bb1003c5254cee4d63fab5a1871a5b9c099c6
nilfont <nilfont@users.noreply.github.com>
2016-07-08 11:03:40 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -46,11 +46,9 @@ function signupAsBasicUser(name, password, email) {
  console.log('pass: '+password);
  console.log('email: '+email);
  user.set("username", name);
- console.log('username ' + user.name);
  user.set("password", password);
- console.log('password '+ user.password);
  user.set("email", email);
- console.log('email '+ user.email);
+ console.log(user);
  console.log('------Abans del user.signUp------');
  return user.signUp().then(function() {
      var query = new Parse.Query(Parse.Role);
