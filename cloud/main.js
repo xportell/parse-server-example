f5cbf81fd4e8c7d19b24714717683600de298270
nilfont <nilfont@users.noreply.github.com>
2016-07-08 10:36:25 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -28,6 +28,7 @@ Parse.Cloud.define("isMe", function(request, response) {
 });
 
 Parse.Cloud.define("signupAsBasicUser", function(request, response) {
+	console.log('request body: '+ request.body);
 	var postParams = request.body;
 	console.log(postParams);
 	signupAsBasicUser(request.params.username, request.params.password, request.params.email).then(function(user) {
