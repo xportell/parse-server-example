4bdf1da9af68e5db102afe0d2ac992a28035dd93
xportell <xportell@confluencia.eu>
2016-08-12 13:19:40 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -37,8 +37,8 @@ Parse.Cloud.afterSave("Postv2",function(request, response) {
 	console.log(response);
 	console.log(request.object.attributes.updatedAt);
 	console.log(request.object.attributes.createdAt);
-	if(request.object.attributes.updatedAt != request.object.attributes.createdAt) updateActivity(request);
-	else response.success('update');
+	if(request.object.attributes.updatedAt == request.object.attributes.createdAt) updateActivity(request);
+	else response.success(request.object); //Not works... the return value is {objectId, createdAt}
 }); 
 
 Parse.Cloud.define('hello', function(req, res) {
