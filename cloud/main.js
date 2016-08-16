a4f0a695b30151c509ccb721d24014d7ed33e527
xportell <xportell@confluencia.eu>
2016-08-16 16:34:54 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -48,7 +48,7 @@ Parse.Cloud.beforeDelete("Postv2", function(request, response) {
   query.find({
     success: function(activity) {
 	var objects = [];
-	console.log(activity);
+	console.log(activity.attributes.childs);
 	_.each(activity.attributes.childs, function(pointer, i) {
 		console.log(pointer);
 		var DObject = Parse.Object.extend(pointer.className);
