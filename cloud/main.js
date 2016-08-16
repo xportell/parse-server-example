8e0933e9d9f04a74ca3dad9dcd22757247102adc
xportell <xportell@confluencia.eu>
2016-08-16 17:05:18 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -52,6 +52,8 @@ Parse.Cloud.beforeDelete("Postv2", function(request, response) {
 	for (var i = 0; i < activities.length; i++) {
 	      var activity = activities[i];
 	      console.log(activity);
+	      console.log(activity.object.atrributes.childs);
+	      console.log(activity.childs);
 	     
 	 }
 	//console.log(activity.get("childs"));
