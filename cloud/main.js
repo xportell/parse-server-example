b1e2bef33e1f15ae2ebd81d7bfd24fda25dd2717
xportell <xportell@confluencia.eu>
2016-12-02 13:31:51 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -235,7 +235,8 @@ function signupAsBasicUser2(name, password, email) {
  user.set("email", email);
  console.log(user);
  console.log('------Abans del user.signUp------');
- return user.signUp({useMasterKey: true}).then(function() {
+ return user.signUp().then(function() {
+	  console.log('------Signup fet------');
      var query = new Parse.Query(Parse.Role);
      query.equalTo("name", 'BasicUser');
      console.log('-----return query.find------');
