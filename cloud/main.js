62094817ed752483f54ab79379630312e99458ee
xportell <xportell@confluencia.eu>
2017-01-24 13:54:50 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -18,11 +18,14 @@ Parse.Cloud.define("getTags", function(request,response){
 	var eventQuery = new Parse.Query("Event");
 	eventQuery.equalTo('tags',2);
 	
+	var forumQuery = new Parse.Query("Forum");
+	forumQuery.equalTo('tags',2);
+	
 	//var query = new Parse.Query(Activity);
 	//query.include("childs");
 	//query.include("childs.author");
 	
-	var query = new Parse.Query.or(eventQuery);
+	var query = new Parse.Query.or(eventQuery, forumQuery);
 
 	query.find({
 		  success: function(activities) {
