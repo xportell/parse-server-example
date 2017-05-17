c793722163af12c554459cccfc720ecd032dfe16
xportell <xportell@confluencia.eu>
2017-05-17 20:26:22 +0200

    Update a4main.js

--- cloud/a4main.js
+++ cloud/a4main.js
@@ -530,7 +530,7 @@ Parse.Cloud.define("changePercent", function(request, response) {
 					console.log(user, role);
 					if(typeof user != 'undefined') id = user.id;
 					else if(typeof role != 'undefined') id = role.id;
-					
+					console.log('+++id',id);
 					if(aIds.indexOf(id) > -1) useMasterKey = true;
 				});
 				//Save ToDo
