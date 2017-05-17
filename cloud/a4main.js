7243f6860c671bf5b9ba66ec98394b93e80134ab
xportell <xportell@confluencia.eu>
2017-05-17 20:32:31 +0200

    Update a4main.js

--- cloud/a4main.js
+++ cloud/a4main.js
@@ -531,7 +531,7 @@ Parse.Cloud.define("changePercent", function(request, response) {
 					if(typeof user != 'undefined') id = user.id;
 					else if(typeof role != 'undefined') id = role.id;
 					if(aIds.indexOf(id) > -1) useMasterKey = true;
-					 console.log('userMasterKey',userMasterKey);
+					 console.log('useMasterKey',useMasterKey);
 				});
 				//Save ToDo
 				  console.log('userMasterKey',userMasterKey);
